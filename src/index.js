190b570bbe4698f566064709b6a66e848839b16a
Toby Ho <airportyh@gmail.com>
2017-05-05 13:28:12 -0400

    Step 1. Connect up the text input to the state.

--- src/index.js
+++ src/index.js
@@ -4,22 +4,45 @@ import * as ReactRedux from 'react-redux';
 import * as Redux from 'redux';
 import './index.css';
 
+function changeQuery(value) {
+  return { type: 'change-query', value: value };
+}
+
 class MovieWidget extends React.Component {
   render() {
-    return <h1>I am movie</h1>;
+    return (
+      <div>
+        <input type="text"
+          value={this.props.query}
+          onChange={event => this.props.changeQuery(event.target.value)}/>
+        <button>Search Movies</button>
+      </div>
+    )
   }
 }
 
 const MovieWidgetContainer = ReactRedux.connect(
-  null,
-  null
+  state => ({ query: state.query }),
+  {
+    changeQuery: changeQuery
+  }
 )(MovieWidget);
 
-function reducer(state = {}, action) {
+const INITIAL_STATE = {
+  query: ''
+};
+function reducer(state = INITIAL_STATE, action) {
+  if (action.type === 'change-query') {
+    return Object.assign({}, state, {
+      query: action.value
+    });
+  }
   return state;
 }
 
-const store = Redux.createStore(reducer);
+const store = Redux.createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <ReactRedux.Provider store={store}>
